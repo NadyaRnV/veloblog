a71d4217fa38baedf212662aff8f0db51a2deb9c
nadyarnv <nadya.rnv@gmail.com>
2016-06-02 22:16:31 +0300

    Кодировочка

--- veloblog/models.py
+++ veloblog/models.py
@@ -1,7 +1,8 @@
+# -*- coding: utf-8 -*-
 from django.db import models
 
 class Post(models.Model):
-    title = models.CharField(max_length=255) # заголовок поста
+    title = models.CharField(max_length=255) # header
     datetime = models.DateTimeField(u'Дата публикации') # дата публикации
     content = models.TextField(max_length=10000) # текст поста
 
